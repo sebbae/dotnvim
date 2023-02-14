183140981e10285dc23d393e7f49fcce8be70e20
Sebastian Baechle <sebastian.baechle@sap.com>
2023-02-14 10:51:39 +0100

    add hint comment and adjust gitignore

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,6 @@
 local builtin = require('telescope.builtin')
+
+-- use <C-Q> to transfer result list to quicklist
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
