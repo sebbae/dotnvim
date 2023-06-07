7bcdb0580ff220038b51e5943847958c23082c61
Sebastian Baechle <sebastian.baechle@sap.com>
2023-06-07 09:37:20 +0200

    improve lsp mappings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -14,8 +14,10 @@ lsp.configure('sumneko_lua', {
 lsp.ensure_installed({
 	'tsserver',
 	'eslint',
-	'sumneko_lua',
-	'rust_analyzer'
+	'lua_ls',
+	'rust_analyzer',
+	'gopls'
+
 })
 
 lsp.set_preferences({
@@ -32,24 +34,28 @@ lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("n", "<leader><leader>", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "<leader>w", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>q", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>r", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>h", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<leader>f", function() vim.lsp.buf.format() end, opts)
+  vim.keymap.set("i", "<C-space>", function() vim.lsp.buf.completion() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp.setup()
 
 vim.diagnostic.config({
-  virtual_text = true,
+  virtual_text = false,
   signs = true,
   update_in_insert = false,
   underline = false,
   severity_sort = false,
-  float = true,
+  float = {
+    source = "always",  -- Or "if_many"
+  }
 })
