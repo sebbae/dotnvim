73ad56ddc012b9802c115530aa44a6e2533c479e
Sebastian Baechle <sebastian.baechle@sap.com>
2023-06-07 09:36:04 +0200

    search file contents with telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,22 @@
 local builtin = require('telescope.builtin')
+local actions = require("telescope.actions")
+
+require("telescope").setup({
+    defaults = {
+        mappings = {
+            i = {
+                ["<esc>"] = actions.close,
+            },
+        },
+    },
+})
 
 -- use <C-Q> to transfer result list to quicklist
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', function()
+    --builtin.find_files({hidden=true})
+    builtin.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})
+end, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>p', builtin.buffers, {})
