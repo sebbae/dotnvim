9fb2352a8fa2dcd0dc5695b0ae199090e0ea5763
Sebastian Baechle <sebastian.baechle@sap.com>
2023-12-27 08:02:37 +0100

    search in open buffers for completions

--- lua/sebbae/init.lua
+++ lua/sebbae/init.lua
@@ -2,3 +2,18 @@ require("sebbae.general")
 require("sebbae.packer")
 require("sebbae.set")
 require("sebbae.netrw")
+require("sebbae.filetype")
+
+-- include all open buffers for completions
+require('cmp').setup {
+  sources = {
+    {
+      name = 'buffer',
+      get_bufnrs = function()
+        return vim.api.nvim_list_bufs()
+      end
+    }
+  }
+}
+
+
