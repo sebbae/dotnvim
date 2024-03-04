211557b72b9453bbc70fb3e8191c7cbd594db3a7
Sebastian Baechle <sebastian.baechle@sap.com>
2024-03-04 09:15:22 +0100

    run black on python files when available

--- lua/sebbae/filetype.lua
+++ lua/sebbae/filetype.lua
@@ -3,3 +3,28 @@ vim.filetype.add({
     cds = 'cds'
   },
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "json",
+	callback = function()
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.tabstop = 2
+	end
+})
+
+vim.api.nvim_create_augroup("AutoFormat", {})
+
+-- autoformat python files on save
+if vim.fn.executable('black') == 1 then
+    vim.api.nvim_create_autocmd(
+        "BufWritePost",
+        {
+            pattern = "*.py",
+            group = "AutoFormat",
+            callback = function()
+                vim.cmd("silent !black --quiet %")
+                vim.cmd("edit")
+            end,
+        }
+    )
+end
