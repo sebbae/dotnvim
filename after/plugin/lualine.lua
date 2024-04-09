e6f2b4e4dc25f2c8502dd5f5b31918370914e984
Sebastian Baechle <sebastian.baechle@sap.com>
2024-04-09 13:53:03 +0200

    fix: diagnostics glyphs in lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -19,7 +19,7 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_b = {'branch', 'diff', {'diagnostics', symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'}}},
     lualine_c = {{'filename', file_status = true, path = 1}},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
