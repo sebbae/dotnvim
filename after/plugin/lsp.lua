ad6f01aef4dde45986db1a2686c6451cabf6b4c1
Sebastian Baechle <sebastian.baechle@sap.com>
2024-04-09 13:53:49 +0200

    feat: pylint support for lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -30,7 +30,9 @@ lsp.set_preferences({
     }
 })
 
-lsp.on_attach(function(client, bufnr)
+lsp.on_attach(on_attach)
+
+local on_attach = function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
@@ -44,8 +46,10 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<leader>f", function() vim.lsp.buf.format() end, opts)
   vim.keymap.set("i", "<C-space>", function() vim.lsp.buf.completion() end, opts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-end)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+end
 
 lsp.setup()
 
@@ -76,6 +80,92 @@ npm i -g @sap/cds-lsp
 }
 require('lspconfig').cds_lsp.setup({})
 
+local function build_pylint_args()
+  local function resolve_cfg_file(filename)
+      local cwd = vim.fn.getcwd()
+      local found = vim.fn.findfile(filename, '.;')
+      if found == "" then
+        return ""
+      else
+          if found:sub(1, 1) == '/' then
+              return found
+          else
+              return (cwd .. '/' .. found)
+          end
+      end
+  end
+
+  local function resolve_directory(directory)
+      local cwd = vim.fn.getcwd()
+      local found = vim.fn.finddir(directory, '.;')
+      if found == "" then
+        return ""
+      else
+          if found:sub(1, 1) == '/' then
+              return found
+          else
+              return (cwd .. '/' .. found)
+          end
+      end
+  end
+
+  local args = {}
+  local pylintrc = resolve_cfg_file(".pylintrc")
+  if pylintrc ~= "" then
+      table.insert(args, "--rcfile=" .. pylintrc)
+  end
+  local source_roots = resolve_directory("src")
+  if source_roots ~= "" then
+      table.insert(args, "--source-roots=" .. source_roots)
+  end
+  return args
+end
+
+local function choose_pylint()
+ local venv_path = os.getenv('VIRTUAL_ENV')
+ if venv_path ~= nil then
+   return venv_path .. "/bin/pylint"
+ end
+ return "pylint"
+end
+
+require('lspconfig').pylsp.setup {
+  on_attach = on_attach,
+  settings = {
+    pylsp = {
+      plugins = {
+        -- formatter options
+        black = { enabled = true },
+        autopep8 = { enabled = false },
+        yapf = { enabled = false },
+        -- linter options
+        pylint = {
+          enabled = true,
+          executable =choose_pylint(),
+          args = build_pylint_args(),
+        },
+        ruff = { enabled = false },
+        pyflakes = { enabled = false },
+        pycodestyle = { enabled = false },
+        -- type checker
+        pylsp_mypy = {
+          enabled = false,
+          overrides = { "--python-executable", py_path, true },
+          report_progress = true,
+          live_mode = false
+        },
+        -- auto-completion options
+        jedi_completion = { fuzzy = false },
+        -- import sorting
+        isort = { enabled = true },
+      },
+    },
+  },
+  flags = {
+    debounce_text_changes = 200,
+  },
+  capabilities = capabilities,
+}
 
 vim.diagnostic.config({
   virtual_text = true,
