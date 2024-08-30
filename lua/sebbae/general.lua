a50239ffddfc44087d904bb7ce311018dde8be89
Sebastian Baechle <sebastian.baechle@sap.com>
2024-08-30 17:37:52 +0200

    feat: adjust j/k keymaps for wrapped lines

--- lua/sebbae/general.lua
+++ lua/sebbae/general.lua
@@ -2,6 +2,9 @@ vim.g.mapleader = ","
 
 vim.keymap.set("n", " ", ":")
 
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
+
 vim.keymap.set("i", "jj", "<esc>", { silent = true })
 
 vim.keymap.set("n", "<tab>", ":bnext<cr>", { silent = true })
